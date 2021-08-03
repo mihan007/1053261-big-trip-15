7ca2a36db3413ecfdc5064216cb4bb43a679cf54
Mikhail Kuklin <mihan007@ya.ru>
2021-08-03 19:49:15 +0300

    Rename renderElement to render because now it's the only function to render html

--- src/main.js
+++ src/main.js
@@ -6,28 +6,28 @@ import Filter from './view/filter';
 import Info from './view/info';
 import Sort from './view/sort';
 import { generatePoint } from './mock/point';
-import { range, renderTemplate, renderElement } from './utils';
+import { range, render } from './utils';
 
 const POINT_COUNT = 20;
 const points = range(1, POINT_COUNT).map(() => generatePoint());
 
 const menuElement = document.querySelector('.js-menu');
-renderElement(menuElement, new Menu().getElement());
+render(menuElement, new Menu().getElement());
 
 const contentElement = document.querySelector('.js-content');
-renderElement(contentElement, new PointForm(points[0]).getElement());
-renderElement(contentElement, new PointForm().getElement());
+render(contentElement, new PointForm(points[0]).getElement());
+render(contentElement, new PointForm().getElement());
 
-points.map((el, index) => renderElement(contentElement, new PointList(points[index]).getElement()));
+points.map((el, index) => render(contentElement, new PointList(points[index]).getElement()));
 
 const tripElement = document.querySelector('.js-trip');
-renderElement(tripElement, new Info(points).getElement());
+render(tripElement, new Info(points).getElement());
 
 const costElement = document.querySelector('.js-cost');
-renderElement(costElement, new Cost(points).getElement());
+render(costElement, new Cost(points).getElement());
 
 const filterElement = document.querySelector('.js-filter');
-renderElement(filterElement, new Filter().getElement());
+render(filterElement, new Filter().getElement());
 
 const sortElement = document.querySelector('.js-sort');
-renderElement(sortElement, new Sort().getElement());
+render(sortElement, new Sort().getElement());
