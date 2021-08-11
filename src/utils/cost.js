4069409700a06ed5262f3e802e65dc0062c3c9c7
Mikhail Kuklin <mihan007@ya.ru>
2021-08-11 10:19:39 +0300

    Remove obsolete variable

--- src/utils/cost.js
+++ src/utils/cost.js
@@ -1,4 +1,4 @@
-export const totalPrice = (points) => points.reduce((prev, item) => {
+export const getFormattedTotalPrice = (points) => points.reduce((prev, item) => {
   const offerPrice = item.offers.reduce((prev, item) => prev + item.price, 0);
   return prev + item.price + offerPrice;
 }, 0);
--- src/view/cost.js
+++ src/view/cost.js
@@ -1,10 +1,10 @@
 import AbstractView from './abstract.js';
-import { totalPrice } from '../utils/cost';
+import { getFormattedTotalPrice } from '../utils/cost';
 
 const createCostTemplate = (points) => {
   return `<section class="trip-main__trip-info  trip-info js-trip">
     <p class="trip-info__cost">
-              Total: €&nbsp;<span class="trip-info__cost-value">${totalPrice(points)}</span>
+              Total: €&nbsp;<span class="trip-info__cost-value">${getFormattedTotalPrice(points)}</span>
       </p>
   </section>
   `;
